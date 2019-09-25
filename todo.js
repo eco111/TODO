43510db770476e793863803232730505bcacc3f7
Eugen Duz <eugen.duz.itp@gmail.com>
2019-09-25 17:21:07 +0300

    Reset color

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+'use strict';
 
 // Remove the default cards if cards are present in the localStorage object
 if (localStorage.cards) {
@@ -13,21 +14,27 @@ const setStorage = () => {
 };
 
 // Wrap or unwrap the card title in del tags for a strikeout effect
-$('.cards').on('click', '#done', () => {
-  if ($(event.target).next().is($('del'))) {
-    $(event.target).next().children().unwrap();
-    setStorage();
-  } else {
-    $(event.target).next().wrap('<del></del>');
-    setStorage();
-  }
-});
+// $('.cards').on('click', '#done', () => {
+//   if ($(event.target). next().is($('del'))) {
+//     $(event.target).next().children().unwrap();
+//     setStorage();
+//   } else {
+//     $(event.target).next().wrap('<del></del>');
+//     setStorage();
+//   }
+// });
 
 // Delete the selected card and update localStorage
-$('.cards').on('click', '#delete', () => {
-  $(event.target).offsetParent().remove();
-  setStorage();
+$('.delete').on('click',
+ () => {
+  console.log("-_-")
+
+  //$(event.target).offsetParent().remove();
+  //setStorage();
 });
+
+// Remove Tile
+
 
 // Append new card on submit and update localStorage
 $('form').on('submit', (event) => {
@@ -37,8 +44,8 @@ $('form').on('submit', (event) => {
     <div class="card">
       <div class="card-block">
           <p class="card-title lead">${title}</p>
-        <button id="done" type="button" class="btn btn-link float-right">❌</button>
-        <button id="delete" type="button" class="btn btn-link float-right mr-3">✔️</button>
+        <button id="delete" type="button" class="btn btn-link float-right">❌</button>
+        <button id="done" type="button" class="btn btn-link float-right mr-3">✔️</button>
       </div>
     </div>
   `;
@@ -56,8 +63,8 @@ $('form').on('click', '#submit', () => {
     <div class="card">
       <div class="card-block">
           <p class="card-title lead">${title}</p>
-        <button id="done" type="button" class="btn btn-link float-right">❌</button>
-        <button id="delete" type="button" class="btn btn-link float-right mr-3">✔️</button>
+        <button id="delete" type="button" class="btn btn-link float-right">❌</button>
+        <button id="done" type="button" class="btn btn-link float-right mr-3">✔️</button>
       </div>
     </div>
   `;
